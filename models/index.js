c9cfa8a175a9dccf5622ade426dedc7f433c6ad6
EmilyBernard <bernam2000@hotmail.com>
2023-03-22 13:19:40 -0400

    cont of models/index.js file

--- models/index.js
+++ models/index.js
@@ -7,7 +7,6 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'CASCADE',
 })
 
 // Categories have many Products
@@ -16,11 +15,15 @@ Category.hasMany(Product,{
 })
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag,{
+  foreignKey: 'ProductTag_id',
+})
 
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product,{
+  foreignKey: 'ProductTag_id',
+})
 
 
 module.exports = {
