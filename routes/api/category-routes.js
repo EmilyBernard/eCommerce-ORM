c2f3ea675fba2ab3ff03c7fb8a514fb86e5d059d
EmilyBernard <bernam2000@hotmail.com>
2023-03-24 12:31:28 -0400

    fixed POST & DELETE code in api/categories-routes.js file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,7 +42,7 @@ router.get('/', async (req, res) => {
 // create a new category
 router.post('/', async (req, res) => {
   try {
-    const categoryData = await category.create(req.body);
+    const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
@@ -74,7 +74,7 @@ router.put('/:id', async (req, res) => {
 // delete a category by its `id` value
 router.delete('/:id', async (req, res) => {
   try {
-    const categoryData = await Category.delete(req.body, {
+    const categoryData = await Category.destroy({
      where: {
           id: req.params.id,
       }
